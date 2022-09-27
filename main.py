ac5233b1c3c1ce713b08de2a7bd70c6c97186688
prathiyunan <114328039+prathiyunan@users.noreply.github.com>
2022-09-27 22:48:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+import random
+from time import sleep
+while(True):
+    temperature=random.randrange(0,100)
+    if(temperature>= 50):
+        
+        humidity=random.randrange(0,20)
+    else:
+        humidity=random.randrange(21,50)
+    print("the temperature of the room is {} fahrenheit".format(temperature))
+
+    print("the humidity of the room is {}".format(humidity))
+
+    if(temperature>60 and humidity<50):
+        print("****the alarm must be detected****")
+        break
+    sleep(2)
